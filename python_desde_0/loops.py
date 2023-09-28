fcfc7fbcf954238fdd404c17ec1ccef30238d3b2
Antonio Sard González <antoniosardgonzalez@gmail.com>
2023-09-28 22:21:21 +0200

    Finished a loop test practice

--- python_desde_0/loops.py
+++ python_desde_0/loops.py
@@ -8,4 +8,16 @@ for x in range(7):
 # Practice exercise with for loops
 partMax = (int)(input("Please introduce the amount of participants: "))
 print ("The system will be configured to accept",partMax,"participants, from now on you can manage this exact number of participants and no more")
-
+for part in range(partMax):
+    inscription = ""
+    while inscription != "y":
+        name = input("Introduce your name: ")
+        email = input("Introduce your email: ")
+        print("Hi",name,"your email is",email,"do you want to confirm the inscription? (Y/N): ")
+        inscription = input().lower()        
+        if inscription == "y":
+            print("Thanks, your inscription is finished.")
+        else:
+            print("Operation cancelled.")
+    print("Your participant number is:",part + 1)
+print("Maximum capacity of participants reached.")
