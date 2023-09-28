564ba4f0ed698f5df01fdaa80206e3d38614830f
Antonio Sard González <antoniosardgonzalez@gmail.com>
2023-09-28 11:37:28 +0200

    first commit

---
+++ python_desde_0/variables.py
@@ -0,0 +1,18 @@
+"""
+Así comentamos en varias líneas de texto
+esto son más comentarios
+variables locas
+"""
+# declaración de alfanuméricos
+nombreAlumno = "Antonio es mi nombre"
+# declaración de enteros
+edadAlumno = 25
+# declaración de decimales
+peso = 63.57
+# declaración de booleanos
+nublado = True
+nublado = False
+nublado = not nublado
+# Aquí imprimimos las variables con cadenas literales alfanuméricas
+print(nombreAlumno, "tengo", edadAlumno, "años. Peso:", peso, "kg.")
+print("¿Está nublado?", nublado)
