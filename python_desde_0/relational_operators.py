3198b61a1d653852b7ec34aac5d16ecd4f9d927b
AnSardG <antoniosardgonzalez@gmail.com>
2023-09-28 16:58:21 +0200

    Testing relational operators

---
+++ python_desde_0/relational_operators.py
@@ -0,0 +1,24 @@
+# equality operator
+print(5==2)
+print(5==5.0)
+print(5=="5")
+# inequality operator
+print (5!=2)
+print (5!="5")
+# lower than operator
+print(5<6)
+"""
+    You cannot use this relational operator like this since it's
+    not supported between instances of 'int' and 'str'
+    
+    print(5<"5")
+"""
+# greater than operator
+print(5>3)
+# print(5>"3")
+# lower or equal than comparator
+print(5<=5)
+# print(5<="5")
+# greater or equal than comparator
+print(5>=5)
+# print(5>="5")
